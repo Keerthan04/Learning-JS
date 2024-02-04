1fba440897f3f1099436daa88fc3247d6902e62a
Keerthan Kumar C <ckeerthankumar4@gmail.com>
2024-02-04 21:12:35 +0530

    learning express routes

--- backend-with-js/express/express routes.js
+++ backend-with-js/express/express routes.js
@@ -94,4 +94,96 @@
     In this code snippet, a .get() route is defined to match /monsters/:name path. When a GET request arrives for /monsters/hydra, the callback is called. Inside the callback, req.params is an object with the key name and the value hydra, which was present in the actual request path.
 
     The appropriate monster is retrieved by name (the object key) from the monsters object and sent back to the client with res.send().
+
+   7.Setting Status Codes
+    Express allows us to set the status code on responses before they are sent. Response codes provide information to clients about how their requests were handled. Until now, we have been allowing the Express server to set status codes for us. For example, any res.send() has by default sent a 200 OK status code.
+
+    The res object has a .status() method to allow us to set the status code, and other methods like .send() can be chained from it.
+
+    const monsterStoreInventory = { fenrirs: 4, banshees: 1, jerseyDevils: 4, krakens: 3 };
+    app.get('/monsters-inventory/:name', (req, res, next) => {
+    const monsterInventory = monsterStoreInventory[req.params.name];
+    if (monsterInventory) {
+        res.send(monsterInventory);
+    } else {
+        res.status(404).send('Monster not found');
+    }
+    });
+
+    In this example, we’ve implemented a route to retrieve inventory levels from a Monster Store. Inventory levels are kept in the monsterStoreInventory variable. When a request arrives for /monsters-inventory/mothMen, the route matches and so the callback is invoked. req.params.name will be equal to 'mothMen' and so our program accesses monsterStoreInventory['mothMen']. Since there are no mothMen in our inventory,res.status() sets a 404 status code on the response, and .send() sends the response.
+
+  8.Matching Longer Paths(how matches for longer paths)
+    Parameters are extremely helpful in making server routes dynamic and able to respond to different inputs. Route parameters will match anything in their specific part of the path, so a route matching /monsters/:name would match all the following request paths:
+
+    /monsters/hydra       //just wont see part will see fully and match
+    /monsters/jörmungandr
+    /monsters/manticore
+    /monsters/123
+
+    In order for a request to match a route path, it must match the entire path, as shown in the diagram to the right. The request arrives for /expressions/1. It first tries to match the /expressions route, but because it has additional path segments after /expressions, it does not match this route and moves on to the next. It matches /expressions/:id because :id will match any value at that level of the path segment. The route matches, so the Express server calls the callback function, which in turn handles the request and sends a response.
+
+  9.Other HTTP Methods
+    HTTP Protocol defines a number of different method verbs with many use cases. So far, we have been using the GET request which is probably the most common of all. Every time your browser loads an image, it is making a GET request for that file!
+
+    This course will cover three other important HTTP methods: PUT, POST, and DELETE. Express provides methods for each one: app.put(), app.post(), and app.delete().
+
+    PUT requests are used for updating existing resources. In our Express Yourself machine, a PUT request will be used to update the name or emoji of an expression already saved in our database. For this reason, we will need to include a unique identifier as a route parameter to determine which specific resource to update.
+
+  10.Using Queries(query parameters)
+    You may have noticed in the previous exercise that our PUT route had no information about how to update the specified expression, just the id of which expression to update. It turns out that there was more information in the request in the form of a query string. Query strings appear at the end of the path in URLs, and they are indicated with a ? character. For instance, in /monsters/1?name=chimera&age=1, the query string is name=chimera&age=1 and the path is /monsters/1/
+
+    Query strings do not count as part of the route path. Instead, the Express server parses them into a JavaScript object and attaches it to the request body as the value of req.query. The key: value relationship is indicated by the = character in a query string, and key-value pairs are separated by &. In the above example route, the req.query object would be { name: 'chimera', age: '1' }.
+
+    const monsters = { '1': { name: 'cerberus', age: '4'  } };
+    // PUT /monsters/1?name=chimera&age=1
+    app.put('/monsters/:id', (req, res, next) => {
+    const monsterUpdates = req.query;
+    monsters[req.params.id] = monsterUpdates;
+    res.send(monsters[req.params.id]);
+    });
+
+    Here, we have a route for updating monsters by ID. When a PUT /monsters/1?name=chimera&age=1 request arrives, our callback function is called and, we create a monsterUpdates variable to store req.query. Since req.params.id is '1', we replace monsters['1']‘s value with monsterUpdates . Finally, Express sends back the new monsters['1'].
+
+    When updating, many servers will send back the updated resource after the updates are applied so that the client has the exact same version of the resource as the server and database.
+
+  11.Matching By HTTP Verb(matching with verb if same route diff put and get matches by http verb)
+    Express matches routes using both path and HTTP method verb. In the diagram to the right, we see a request with a PUT verb and /expressions (remember that the query is not part of the route path). The path for the first route matches, but the method verb is wrong, so the Express server will continue to the next registered route. This route matches both method and path, and so its callback is called, the necessary updating logic is executed, and the response is sent.
+ 
+  12.Creating An Expression
+    POST is the HTTP method verb used for creating new resources. Because POST routes create new data, their paths do not end with a route parameter, but instead end with the type of resource to be created.
+
+    For example, to create a new monster, a client would make a POST request to /monsters. The client does not know the id of the monster until it is created and sent back by the server, therefore POST /monsters/:id doesn’t make sense because a client couldn’t know the unique id of a monster before it exists.
+
+    First, we create a new element using createElement() from utils.js:
+
+    app.post('/monsters', (req, res, next) => {
+    const receivedExpression = createElement('monsters', req.query);
+
+    If our new element is valid, we push() it to our array of elements:
+
+    monsters.push(receivedExpression);
+
+    Express uses .post() as its method for POST requests. POST requests can use many ways of sending data to create new resources, including query strings.
+
+    The HTTP status code for a newly-created resource is 201 Created.
+
+  13.Deleting Old Expressions
+    DELETE is the HTTP method verb used to delete resources. Because DELETE routes delete currently existing data, their paths should usually end with a route parameter to indicate which resource to delete.
+
+    First, we get the index of the resource using getIndexById() from utils.js:
+
+    app.delete('/monsters/:id', (req, res, next) => {
+    const expressionIndex = getIndexById(req.params.id, monsters);
+
+    If the index is valid, we use splice() to remove the element at that index:
+
+    monsters.splice(expressionIndex, 1);
+
+    Express uses .delete() as its method for DELETE requests.
+
+    Servers often send a 204 No Content status code if deletion occurs without error.
+    Wrap Up
+
+    
+In this exercise, you were able to create a full server allowing users to implement all CRUD operations for two kinds of resources: Expressions and Animals! With these skills and knowledge of the HTTP request-response cycle, you could implement an API for any project needing CRUD functionality. You could build a trip planner, an address book, a grocery list, an image-sharing application, an anonymous message board, the sky’s the limit!
 */
--- backend-with-js/express/start-my-first-express-server.js
+++ backend-with-js/express/start-my-first-express-server.js
@@ -1,10 +1,11 @@
 const express=require('express');//first install express by npm install express
 const app=express();
+//learn sending post put requests because browser send get requests mostly
 
 //uses as static-learn what it means
 app.use(express.static('public'));
 
-//startin my routes(to tell how and diff requests handler with help of path and http verb)
+//starting my routes(to tell how and diff requests handler with help of path and http verb)
 //home route(/)(always when started this route)
 app.get('/',(req,res)=>{
     console.log("request revieved");
@@ -20,21 +21,54 @@ app.get('/about',(req,res)=>{
     //console.log(res);//seeing my response object
 })
 //created a dummy route
-const students={
+let students={
     1:{name:"keerthana",course:"bca"},
     2:{name:"prarthana",course:"bca"},
     3:{name:"Jayalaxmi",course:"bca"},
 }
 app.get('/students',(req,res)=>{
-    res.send(students)
+    res.send(students);
 })
 //creating a dynamic route
-app.get('/students/:id',(req,res)=>{
-    console.log(req.params)//the object req.params will have now {id : "whatever id passed in route"}
-    req.send(students[req.params.id]);//we are using it to extract the id from the url(access property and use it as a key)
+app.get('/students/:id',(req,res)=>{//params always has : before them in route
+    //the object req.params will have now {id : "whatever id passed in route"}
+    //setting status codes and sending response
+    const getbyid= students[req.params.id]
+    if(getbyid){
+        res.send(students[req.params.id]);//we are using it to extract the id from the url(access property and use it as a key)
+        //res.send(`ur id is ${req.params.id}`);
+        //always sendin is res.send
+    }
+    else{
+        res.status(404).send("id not found");//setting status code so client can see about their response
+    }
+    
 })
-app.listen(3000,()=>{
-    console.log('server is running on port 3000');
+//using query parameters and put request
+app.put('/students/:id', (req, res) => {
+    const query = req.query;
+    console.log(query);
+    students[req.params.id] = query;
+    console.log(students);
+    res.send(students[req.params.id]);
+});//see how query comes
+//http..../students/3?name=chandru&course=mca so query object as key value pairs the = wala and & is another property
+//the query is {name:chandru,course:mca} so based on the id we set it(update as put request) and we send the updated one always to client
+
+//creating a post route(remember id and all wont be there)
+app.post('/students',(req,res)=>{//status of 201 if success or else 400
+    let query=req.query;
+    students[4]=query;//here simply done actually dynamically shd be created
+    res.send(students);
+})
+
+//creating delete route
+qpp.delete('/students/:id',(req,res)=>{//id as delete of id if sucess status 204
+    delete students[req.params.id];
+    res.send(students);
+})
+app.listen(4000,()=>{
+    console.log('server is running on port 4000');
 })
 //to stop in teminal type ctrl + c and always start server  and see
 //whenever a change is made here wont reflect in already running server so shd restart server ctrl+c then restart
